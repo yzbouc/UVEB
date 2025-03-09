a9c38b284aa9fca609b443c1857ecd8468ce7a7d
yzbouc <66795226+yzbouc@users.noreply.github.com>
2025-03-09 23:39:17 +0800

    Update setup.py

--- UVE-Net/setup.py
+++ UVE-Net/setup.py
@@ -47,7 +47,7 @@ def get_hash():
         sha = get_git_hash()[:7]
     elif os.path.exists(version_file):
         try:
-            from basicsr.version import __version__
+            __version__='1.3.3.10'
             sha = __version__.split('+')[-1]
         except ImportError:
             raise ImportError('Unable to get git version')
